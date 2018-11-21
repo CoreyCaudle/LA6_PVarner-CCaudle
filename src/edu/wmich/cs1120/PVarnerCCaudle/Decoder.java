f9ab6be4c240d8c17d65104ecf201931d5b3eb0a
coreycaudle <coreycaudle@wireless30-232.wireless.wmich.edu>
2018-11-20 22:11:47 -0500

    Corey

---
+++ src/edu/wmich/cs1120/PVarnerCCaudle/Decoder.java
@@ -0,0 +1,11 @@
+package edu.wmich.cs1120.PVarnerCCaudle;
+
+public class Decoder implements IDecoder{
+
+	@Override
+	public void decode(String filePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/edu/wmich/cs1120/PVarnerCCaudle/Encoder.java
@@ -0,0 +1,11 @@
+package edu.wmich.cs1120.PVarnerCCaudle;
+
+public class Encoder implements IEncoder{
+
+	@Override
+	public void encode(String inputFileName, String outputFilePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
